607adef71f4a37be6070ea6c597c65f63fce0866
maneSidd <siddharth.mane@mitaoe.ac.in>
2024-02-22 18:59:34 +0530

    DbContext updated

--- Backend/VendorPortal.API/Data/VendorPortalDbContext.cs
+++ Backend/VendorPortal.API/Data/VendorPortalDbContext.cs
@@ -1,14 +1,27 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using System.Reflection.Emit;
+﻿using Microsoft.EntityFrameworkCore;
+using VendorPortal.API.Models.Domain;
 
-namespace VentorPortal.API.Data
+namespace VendorPortal.API.Data
 {
-    public class VentorPortalDbContext : IdentityDbContext
+    public class VendorPortalDbContext : DbContext
     {
-        public VentorPortalDbContext(DbContextOptions<VentorPortalDbContext> options) : base(options)
+        public VendorPortalDbContext(DbContextOptions<VendorPortalDbContext> options) : base(options)
         {
         }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<RFP> RFPs { get; set; }
+        public DbSet<VendorCategory> VendorCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .HasKey(e => e.Id);
+            modelBuilder.Entity<VendorCategory>()
+                .HasKey(e => new { e.VendorId,e.CategoryId});
+        }
+
+
     }
 }
