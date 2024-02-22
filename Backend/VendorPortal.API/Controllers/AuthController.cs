c5d86a9a8ab89b7893be4f47c558577c2d795418
maneSidd <siddharth.mane@mitaoe.ac.in>
2024-02-22 11:31:12 +0530

    controllers are added

---
+++ Backend/VendorPortal.API/Controllers/AuthController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using VendorPortal.API.Data;
+using VendorPortal.API.Models.Domain;
+using VendorPortal.API.Models.Dto;
+
+namespace VendorPortal.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]         
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<UserProfile> userManager;
+
+        public AuthController(UserManager<UserProfile> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        
+        [HttpPost]
+        [Route("Register/Vendor")]
+        public async Task<IActionResult> VendorRegistration([FromBody] VendorDto vendorDto)
+        {
+            var vendor = new UserProfile
+            {
+                Name = vendorDto.Organization,
+                UserName = vendorDto.Organization,
+                Organization = vendorDto.Organization,
+                ContactPerson = vendorDto.ContactPerson,
+                Email = vendorDto.Email,
+                PhoneNumber = vendorDto.PhoneNumber,
+                State = vendorDto.State,
+                City = vendorDto.City,
+                Address = vendorDto.Address,
+                Pincode = vendorDto.Pincode,
+                Category = vendorDto.Category
+            };
+
+            var vendorResult=await userManager.CreateAsync(vendor, vendorDto.Password);
+
+            if (vendorResult.Succeeded)
+            {
+                string []roles = ["Vendor"];
+                vendorResult = await userManager.AddToRolesAsync(vendor, roles);
+                if (vendorResult.Succeeded)
+                {
+                    return Ok(" Vendor is added !! ");
+                }
+                return BadRequest("Error, Roles is not added !!");
+             }
+
+            return BadRequest(vendorResult);
+        }
+
+        [HttpPost]
+        [Route("Register/ProjectHead")]
+        public async Task<IActionResult> ProjectHeadRegistration([FromBody] ProjectHeadDto projectHeadDto)
+        {
+            var projectHead = new UserProfile
+            {
+                Name = projectHeadDto.Name,
+                UserName = projectHeadDto.UserName,
+                Email = projectHeadDto.Email,
+                PhoneNumber = projectHeadDto.PhoneNumber 
+            };
+            var response = await userManager.CreateAsync(projectHead,projectHeadDto.Password);
+            if (response.Succeeded)
+            {
+                string[] roles = ["ProjectHead"];
+                response = await userManager.AddToRolesAsync(projectHead,roles);
+                if (response.Succeeded)
+                {
+                    return Ok("Project Head is Created !! ");
+                }
+                return BadRequest("Can not Assign Role to Project Head !! ");
+            }
+            return BadRequest("Error, Can not create Project Head !!");
+        }
+
+
+        [HttpPost]
+        [Route("Login/Vendor")]
+        public async Task<IActionResult> VendorLogin([FromBody] VendorLoginDto vendorLogin)
+        {
+            UserProfile vendor = await userManager.FindByEmailAsync(vendorLogin.Email);
+            
+
+            if(vendor == null)
+            {
+                return BadRequest("No Records Found !! ");
+            }
+            var response = await userManager.CheckPasswordAsync(vendor, vendorLogin.Password);
+            if (response)
+            {
+                return Ok("Vendor is Looged In !!");
+            }
+            return BadRequest("Unable to Log In !!");
+
+        }
+
+
+        [HttpPost]
+        [Route("Login/ProjectHead")]
+        public async Task<IActionResult> ProjectLoginLogin([FromBody] ProjectHeadLoginDto projectHead)
+        {
+            UserProfile head = await userManager.FindByEmailAsync(projectHead.Email);
+
+            if (head == null)
+            {
+                return BadRequest("No Records Found !! ");
+            }
+            var response = await userManager.CheckPasswordAsync(head, projectHead.Password);
+            if (response)
+            {
+                return Ok("Project Head is Looged In !!");
+            }
+            return BadRequest("Unable to Log In !!");
+
+        }
+
+
+    }
+}
---
+++ Backend/VendorPortal.API/Controllers/ProjectHeadsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VendorPortal.API.Models.Domain;
+using VendorPortal.API.Models.Dto;
+
+namespace VendorPortal.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectHeadsController : ControllerBase
+    {
+        private readonly UserManager<UserProfile> userManager;
+
+        public ProjectHeadsController(UserManager<UserProfile> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        [Route("{Id}")]
+        public async Task<IActionResult> GetById([FromRoute] string Id)
+        {
+            UserProfile user = await userManager.Users.FirstOrDefaultAsync(x => x.Id == Id);
+            if (user == null)
+            {
+                return BadRequest("Error, Invalid Id !!");
+            }
+            var userDto = new ProjectHeadDto
+            {
+                Name = user.Name,
+                UserName = user.UserName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber
+            };
+            return Ok(userDto);
+        }
+
+        [HttpPut]
+        [Route("{Id}")]
+        public async Task<IActionResult> Update([FromRoute] string Id, [FromBody] ProjectHeadUpdateDto projectHead)
+        {
+            UserProfile user = await userManager.FindByIdAsync(Id);
+            if (user == null)
+            {
+                return BadRequest("Error, Invalid Id !!");
+            }
+ 
+            user.PhoneNumber = projectHead.PhoneNumber;
+            user.Name = projectHead.Name;
+
+            var result = await userManager.UpdateAsync(user);
+
+            if (result.Succeeded)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
+        }
+
+
+
+    }
+}
