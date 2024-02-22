418c1ea4c88e69b6ef92fb34c62b90e94322ee15
maneSidd <siddharth.mane@mitaoe.ac.in>
2024-02-22 11:26:19 +0530

    Dto folder is added

---
+++ Backend/VendorPortal.API/Models/Dto/VendorDto.cs
@@ -0,0 +1,17 @@
+﻿namespace VendorPortal.API.Models.Dto
+{
+    public class VendorDto
+    {
+        public string Organization { get; set; }
+        public string ContactPerson { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public int Pincode { get; set; }
+        public string Category { get; set; }
+        public string Password { get; set; }
+
+    }
+}
