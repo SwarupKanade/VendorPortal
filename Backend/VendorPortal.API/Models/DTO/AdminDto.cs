6c55ff413a42973c9c5c9c0a7ae8014a4d477ef5
maneSidd <siddharth.mane@mitaoe.ac.in>
2024-02-29 13:00:39 +0530

    datatype added in admin dto

--- Backend/VendorPortal.API/Models/DTO/AdminDto.cs
+++ Backend/VendorPortal.API/Models/DTO/AdminDto.cs
@@ -4,8 +4,14 @@
     {
         
         public string Name { get; set; }
+        
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Invalid Mobile Number")]
         public string PhoneNumber { get; set; }
+
+        [DataType(DataType.Password, ErrorMessage = "Uppercase,Symbol & Numbers combination required")]
         public string Password { get; set; }
 
     }
