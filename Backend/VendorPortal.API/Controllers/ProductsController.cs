00b4be88bd6d3bc722e1358552993b735db21b46
maneSidd <siddharth.mane@mitaoe.ac.in>
2024-03-04 16:06:58 +0530

    Product Controller added

---
+++ Backend/VendorPortal.API/Controllers/ProductsController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VendorPortal.API.Data;
+using VendorPortal.API.Models.Domain;
+using VendorPortal.API.Models.DTO;
+
+namespace VendorPortal.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly VendorPortalDbContext dbContext;
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        public ProductsController(VendorPortalDbContext dbContext ,IWebHostEnvironment webHostEnvironment, IHttpContextAccessor httpContextAccessor)
+        {
+            this.dbContext = dbContext;
+            this.webHostEnvironment = webHostEnvironment;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Add( [FromForm] ProductDto productDto) {
+
+            ValidateFileUpload(productDto.FormFile);
+            string path= await Upload(productDto.FormFile);
+
+            var productDomain = new Product { 
+                Name = productDto.Name,
+                ImagePath = path,
+                LongDescription = productDto.LongDescription,
+                ShortDescription = productDto.ShortDescription,
+                UnitType = productDto.UnitType,
+                Specification = productDto.Specification,
+                ProductCategory = productDto.ProductCategory,
+                SubCategory = productDto.SubCategory,
+                SizeId = productDto.SizeId
+            };
+
+            await dbContext.Products.AddAsync(productDomain);
+            await dbContext.SaveChangesAsync();
+            return Ok(productDomain);
+        }
+        private async Task<string> Upload(IFormFile document)
+        {
+            var folder = Path.Combine(webHostEnvironment.ContentRootPath, "ProductImages");
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+            var localFilePath = Path.Combine(folder, document.FileName);
+
+            using var stream = new FileStream(localFilePath, FileMode.Create);
+            await document.CopyToAsync(stream);
+            var urlFilePath = $"{httpContextAccessor.HttpContext.Request.Scheme}://{httpContextAccessor.HttpContext.Request.Host}{httpContextAccessor.HttpContext.Request.PathBase}/ProductImages/{document.FileName}";
+            var FilePath = urlFilePath;
+            return FilePath;
+        }
+
+        private void ValidateFileUpload(IFormFile document)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png", ".pdf" };
+
+            if (!allowedExtensions.Contains(Path.GetExtension(document.FileName)))
+            {
+                ModelState.AddModelError("file", "Unsupported file extension");
+            }
+
+            if (document.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File size more than 10MB, please upload a smaller size file.");
+            }
+        }
+
+    }
+}
