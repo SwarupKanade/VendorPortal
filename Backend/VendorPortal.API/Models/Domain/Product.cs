3db21f72e639231598076a2a2851f57b54a237fe
maneSidd <siddharth.mane@mitaoe.ac.in>
2024-03-04 16:08:17 +0530

    Files added in domain

---
+++ Backend/VendorPortal.API/Models/Domain/Product.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+
+namespace VendorPortal.API.Models.Domain
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        [NotMapped]
+        public IFormFile FormFile { get; set; }
+        public string ImagePath { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? LongDescription { get; set; }
+        public string UnitType { get; set; }
+        public string? Specification { get; set; }
+        public string? ProductCategory { get; set; }
+        public string? SubCategory { get; set; }
+
+        [ForeignKey("Id")]
+        public Guid  SizeId { get; set; }
+        public ProductSize ProductSize { get; set; }
+    }
+}
---
+++ Backend/VendorPortal.API/Models/Domain/ProductSize.cs
@@ -0,0 +1,9 @@
+﻿namespace VendorPortal.API.Models.Domain
+{
+    public class ProductSize
+    {
+        public Guid Id { get; set; }
+        public string SizeName { get; set; }
+        public string UnitType { get; set; }
+    }
+}
